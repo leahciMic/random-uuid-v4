178939fae3ca6748a0e01d9746a1c3db7811b1d0
Michael Leaney <leahcimic@gmail.com>
2014-04-09 11:20:18 +1000

    Updated to use Briguy37's modification

--- uuidv4.js
+++ uuidv4.js
@@ -1,15 +1,21 @@
 /**
- * This function was taken from a stackoverflow answer.
- * 
- * All credit goes to broofa (http://stackoverflow.com/users/109538/broofa)
+ *
+ * This function was taken from a stackoverflow answer:
  *
  * http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+ *
+ * Many thanks to:
+ *
+ * Briguy37 (http://stackoverflow.com/users/508537/briguy37)
+ * broofa (http://stackoverflow.com/users/109538/broofa)
  *
  */
 
 module.exports = function() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-      return v.toString(16);
-  });
+    var d = new Date().getTime();
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+    });
 };
